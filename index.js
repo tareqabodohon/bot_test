ed74e8af8c9f7411be6ca0c2bdf265259eb55741
tarekabodohon <tarekabod30@gmail.com>
2022-11-20 16:10:55 +0300

    add verify

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+// Add support for GET requests to our webhook
+app.get("/webhook", (req, res) => {
+   const pageToken = "test";
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+    if(token===pageToken){
+
+    }
+    else{
+        res.status(403).send();
+    }
+    });
+
+
 app.get("/",function (req,res){
     res.send("Hey!! I'm just a chatbot web app.");
 });
